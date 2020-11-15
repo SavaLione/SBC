c960ce10619462ded9ebea6adceb0d4c6121f8dd
SavaLione <monologuesplus@gmail.com>
2020-11-15 17:54:13 +0500

    client

--- src/core/sbc_client.cpp
+++ src/core/sbc_client.cpp
@@ -73,15 +73,16 @@ int main(int argc, char *argv[])
         std::cout << "Enter message: ";
         char request[max_length];
         std::cin.getline(request, max_length);
-        size_t request_length = strlen(request);
-        boost::asio::write(s, boost::asio::buffer(request, request_length));
+        //size_t request_length = strlen(request);
+        boost::asio::write(s, boost::asio::buffer(request, max_length));
 
         char reply[max_length];
-        size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, request_length));
-        // std::cout.write(reply, reply_length);
-        // std::cout << "\n";
+        size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, max_length));
         std::cout << "Reply is:" << std::endl;
-        std::cout.write(reply, 16);
+        std::cout.write(reply, reply_length);
+        // std::cout << "\n";
+        //std::cout << "Reply is:" << std::endl;
+        //std::cout.write(reply, max_length);
         std::cout << std::endl;
     }
     catch (std::exception &e)
