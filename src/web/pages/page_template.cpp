c6341dc7e868034581be2c1fb3f19352575c4a20
SavaLione <monologuesplus@gmail.com>
2020-12-08 01:26:04 +0500

    page

--- src/web/pages/page_template.cpp
+++ src/web/pages/page_template.cpp
@@ -89,7 +89,7 @@ void page_template::_head()
     _add_content("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
     _add_content("<meta name=\"description\" content=\"\">");
     _add_content("<meta name=\"author\" content=\"SavaLione\">");
-    _add_content("<link rel=\"icon\" href=\"../favicon.ico\">");
+    _add_content("<link rel=\"icon\" href=\"assets/favicon.ico\">");
     _add_content("");
     {
         std::string s = "<title>";
@@ -100,7 +100,7 @@ void page_template::_head()
     _add_content("");
     _add_content("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">");
     _add_content("");
-    _add_content("<link href=\"form-validation.css\" rel=\"stylesheet\">");
+    _add_content("<link href=\"assets/form-validation.css\" rel=\"stylesheet\">");
     _add_content("");
 
     _add_content("</head>");
