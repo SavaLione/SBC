ffa2cd7e900ba691ac5e7d50e01f486fdc167379
SavaLione <monologuesplus@gmail.com>
2020-12-12 21:22:50 +0500

    request handler

--- src/web/request_handler.cpp
+++ src/web/request_handler.cpp
@@ -130,7 +130,7 @@ void request_handler::_recognize_user()
 
         /* Пытаемся получить пользователя по uuid */
         /* Тут надо исправить, упор идёт на uuid, а мы ещё можем обрабатывать username */
-        _user._uuid = _cookie.get_uuid();
+        _user._uuid = _cookie.get_uuid().value;
         _user_handler.get();
     }
     else
