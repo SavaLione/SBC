42a56b72993cfddf47215ead598116e7a5946218
SavaLione <monologuesplus@gmail.com>
2021-01-12 13:48:37 +0500

    ddl_type

--- src/db/db.cpp
+++ src/db/db.cpp
@@ -83,11 +83,7 @@ void db::create()
             soci::ddl_type ddl = sql.create_table("users");
 
             ddl.column("id", soci::dt_integer)("not null");
-
-            // ddl.column("username", soci::dt_string)("not null unique");
-            ddl.column("username", soci::dt_string)("not null");
-            // ddl.unique("users", "username");
-
+            ddl.column("username", soci::dt_string)("not null unique");
             ddl.column("password", soci::dt_string);
             ddl.column("name", soci::dt_string);
             ddl.column("email", soci::dt_string);
