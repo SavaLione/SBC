f4e43824709a75f5beb3d1ade971b7ccaf574650
SavaLione <monologuesplus@gmail.com>
2021-01-14 09:51:56 +0500

    Изменение страницы регистрации в коде

--- src/web/pages/registration.cpp
+++ src/web/pages/registration.cpp
@@ -43,24 +43,24 @@ registration::~registration()
 
 void registration::_head()
 {
-    _add_content("  <head>");
+    _add_content("<head>");
 
-    _add_content("    <meta charset=\"utf-8\">");
-    _add_content("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
-    _add_content("    <meta name=\"description\" content=\"\">");
-    _add_content("    <meta name=\"author\" content=\"\">");
-    _add_content("    <link rel=\"icon\" href=\"../favicon.ico\">");
+    _add_content("  <meta charset=\"utf-8\">");
+    _add_content("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
+    _add_content("  <meta name=\"description\" content=\"\">");
+    _add_content("  <meta name=\"author\" content=\"\">");
+    _add_content("  <link rel=\"icon\" href=\"/assets/favicon.ico\">");
 
-    _add_content("    <title>Registration</title>");
+    _add_content("  <title>Registration</title>");
 
-    _add_content("    <!-- Bootstrap core CSS -->");
-    _add_content("    <!-- <link href=\"../dist/css/bootstrap.min.css\" rel=\"stylesheet\"> -->");
-    _add_content("    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">");
+    _add_content("  <!-- Bootstrap core CSS -->");
+    _add_content("  <!-- <link href=\"../dist/css/bootstrap.min.css\" rel=\"stylesheet\"> -->");
+    _add_content("  <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">");
 
-    _add_content("    <!-- Custom styles for this template -->");
-    _add_content("    <link href=\"form-validation.css\" rel=\"stylesheet\">");
+    _add_content("  <!-- Custom styles for this template -->");
+    _add_content("  <link href=\"/assets/css/registration.css\" rel=\"stylesheet\">");
 
-    _add_content("  </head>");
+    _add_content("</head>");
 }
 
 void registration::_body()
@@ -82,11 +82,12 @@ void registration::_body()
     else
     {
         /* Пользователя нет в системе */
-        _add_content("  <body class=\"bg-light\">");
+        _add_content("<body class=\"bg-light\">");
+        _add_content("  <form method=\"post\" class=\"form-signin\">");
 
         _add_content("    <div class=\"container\">");
         _add_content("      <div class=\"py-5 text-center\">");
-        _add_content("        <img class=\"d-block mx-auto mb-4\" src=\"../console.svg\" alt=\"\" width=\"72\" height=\"72\">");
+        _add_content("        <img class=\"d-block mx-auto mb-4\" src=\"/assets/console.svg\" alt=\"\" width=\"72\" height=\"72\">");
         _add_content("        <h2>Registration form</h2>");
         _add_content("        <p class=\"lead\">Registration form for SBC service.</p>");
         _add_content("      </div>");
@@ -99,21 +100,21 @@ void registration::_body()
         _add_content("            <div class=\"row\">");
         _add_content("              <div class=\"col-md-4 mb-3\">");
         _add_content("                <label for=\"lastName\">Last name</label>");
-        _add_content("                <input type=\"text\" class=\"form-control\" id=\"lastName\" placeholder=\"\" value=\"\" required>");
+        _add_content("                <input type=\"text\" class=\"form-control\" id=\"input_last_name\" placeholder=\"\" value=\"\" required>");
         _add_content("                <div class=\"invalid-feedback\">");
         _add_content("                  Valid last name is required.");
         _add_content("                </div>");
         _add_content("              </div>");
         _add_content("              <div class=\"col-md-4 mb-3\">");
         _add_content("                <label for=\"firstName\">First name</label>");
-        _add_content("                <input type=\"text\" class=\"form-control\" id=\"firstName\" placeholder=\"\" value=\"\" required>");
+        _add_content("                <input type=\"text\" class=\"form-control\" id=\"input_first_name\" placeholder=\"\" value=\"\" required>");
         _add_content("                <div class=\"invalid-feedback\">");
         _add_content("                  Valid first name is required.");
         _add_content("                </div>");
         _add_content("              </div>");
         _add_content("              <div class=\"col-md-4 mb-3\">");
         _add_content("                <label for=\"middleName\">Middle name</label>");
-        _add_content("                <input type=\"text\" class=\"form-control\" id=\"middleName\" placeholder=\"\" value=\"\" required>");
+        _add_content("                <input type=\"text\" class=\"form-control\" id=\"input_middle_name\" placeholder=\"\" value=\"\" required>");
         _add_content("                <div class=\"invalid-feedback\">");
         _add_content("                  Valid middle name is required.");
         _add_content("                </div>");
@@ -126,7 +127,7 @@ void registration::_body()
         _add_content("                <div class=\"input-group-prepend\">");
         _add_content("                  <span class=\"input-group-text\">@</span>");
         _add_content("                </div>");
-        _add_content("                <input type=\"text\" class=\"form-control\" id=\"username\" placeholder=\"Username\" required>");
+        _add_content("                <input type=\"text\" class=\"form-control\" id=\"input_username\" placeholder=\"Username\" required>");
         _add_content("                <div class=\"invalid-feedback\" style=\"width: 100%;\">");
         _add_content("                  Your username is required.");
         _add_content("                </div>");
@@ -135,7 +136,7 @@ void registration::_body()
 
         _add_content("            <div class=\"mb-3\">");
         _add_content("              <label for=\"email\">Email <span class=\"text-muted\"></span></label>");
-        _add_content("              <input type=\"email\" class=\"form-control\" id=\"email\" placeholder=\"you@example.com\" required>");
+        _add_content("              <input type=\"email\" class=\"form-control\" id=\"input_email\" placeholder=\"you@example.com\" required>");
         _add_content("              <div class=\"invalid-feedback\">");
         _add_content("                Please enter a valid email address for shipping updates.");
         _add_content("              </div>");
@@ -145,7 +146,7 @@ void registration::_body()
 
         _add_content("            <div class=\"mb-3\">");
         _add_content("              <label for=\"address\">Address <span class=\"text-muted\">(Optional)</span></label>");
-        _add_content("              <input type=\"text\" class=\"form-control\" id=\"address\" placeholder=\"1234 Main St\">");
+        _add_content("              <input type=\"text\" class=\"form-control\" id=\"input_address\" placeholder=\"1234 Main St\">");
         _add_content("              <div class=\"invalid-feedback\">");
         _add_content("                Your address.");
         _add_content("              </div>");
@@ -153,13 +154,13 @@ void registration::_body()
 
         _add_content("            <div class=\"mb-3\">");
         _add_content("              <label for=\"address2\">phone <span class=\"text-muted\">(Optional)</span></label>");
-        _add_content("              <input type=\"text\" class=\"form-control\" id=\"phone\" placeholder=\"Phone number\">");
+        _add_content("              <input type=\"text\" class=\"form-control\" id=\"input_phone\" placeholder=\"Phone number\">");
         _add_content("            </div>");
 
         _add_content("            <div class=\"row\">");
         _add_content("              <div class=\"col-md-5 mb-3\">");
         _add_content("                <label for=\"country\">Branch of the company <span class=\"text-muted\">(Optional)</span></label>");
-        _add_content("                <select class=\"custom-select d-block w-100\" id=\"branch\">");
+        _add_content("                <select class=\"custom-select d-block w-100\" id=\"input_branch\">");
         _add_content("                  <option value=\"\">Not set</option>");
         _add_content("                  <option>Yekaterinburg</option>");
         _add_content("                  <option>Moscow</option>");
@@ -172,7 +173,7 @@ void registration::_body()
         _add_content("              </div>");
         _add_content("              <div class=\"col-md-4 mb-3\">");
         _add_content("                <label for=\"state\">The Department</label>");
-        _add_content("                <select class=\"custom-select d-block w-100\" id=\"department\">");
+        _add_content("                <select class=\"custom-select d-block w-100\" id=\"input_department\">");
         _add_content("                  <option value=\"\">Not set</option>");
         _add_content("                  <option value=\"\">Delivery</option>");
         _add_content("                  <option>Unpacking</option>");
@@ -186,7 +187,7 @@ void registration::_body()
         _add_content("            </div>");
         _add_content("            <hr class=\"mb-4\">");
         _add_content("            <div class=\"custom-control custom-checkbox\">");
-        _add_content("              <input type=\"checkbox\" class=\"custom-control-input\" id=\"terms-of-use\" required>");
+        _add_content("              <input type=\"checkbox\" class=\"custom-control-input\" id=\"input_terms_of_use\" required>");
         _add_content("              <label class=\"custom-control-label\" for=\"terms-of-use\">I agree to the terms of use</label>");
         _add_content("            </div>");
         _add_content("            <hr class=\"mb-4\">");
@@ -200,7 +201,7 @@ void registration::_body()
         _add_content("        <p class=\"mb-1\">&copy; 2021 SBC team</p>");
         _add_content("        <ul class=\"list-inline\">");
         _add_content("          <li class=\"list-inline-item\"><a href=\"/about\">Privacy</a></li>");
-        _add_content("          <li class=\"list-inline-item\"><a href=\"/about\">Terms</a></li>");
+        _add_content("          <li class=\"list-inline-item\"><a href=\"/terms\">Terms</a></li>");
         _add_content("          <li class=\"list-inline-item\"><a href=\"/about\">Support</a></li>");
         _add_content("        </ul>");
         _add_content("      </footer>");
@@ -210,7 +211,9 @@ void registration::_body()
         _add_content("    ================================================== -->");
         _add_content("    <!-- Placed at the end of the document so the pages load faster -->");
         _add_content("    <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
-        _add_content("    <script>window.jQuery || document.write('<script src=\"../assets/js/vendor/jquery-slim.min.js\"><\\/script>')</script>");
+        _add_content("    <script>");
+        _add_content("      window.jQuery || document.write('<script src=\"../assets/js/vendor/jquery-slim.min.js\"><\\/script>')");
+        _add_content("    </script>");
         _add_content("    <!-- <script src=\"../assets/js/vendor/popper.min.js\"></script> -->");
         _add_content("    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.5.4/umd/popper.min.js\" integrity=\"sha512-7yA/d79yIhHPvcrSiB8S/7TyX0OxlccU8F/kuB8mHYjLlF1MInPbEohpoqfz0AILoq5hoD7lELZAYYHbyeEjag==\" crossorigin=\"anonymous\"></script>");
         _add_content("    <!-- <script src=\"../dist/js/bootstrap.min.js\"></script> -->");
@@ -239,6 +242,7 @@ void registration::_body()
         _add_content("        }, false);");
         _add_content("      })();");
         _add_content("    </script>");
-        _add_content("  </body>");
+        _add_content("  </form>");
+        _add_content("</body>");
     }
 }
