743dc9afadd86f51cb632ab9e774609bba485814
SavaLione <monologuesplus@gmail.com>
2021-01-15 10:31:28 +0500

    sbc-client Добавлены новые функции и поля
    + username
    + password
    + get_line - получить параметр из консоли
    + hide_password - заменить пароль на *
    * str_request - строка с запросом

--- src/core/sbc_client.cpp
+++ src/core/sbc_client.cpp
@@ -45,13 +45,20 @@
 
 using boost::asio::ip::tcp;
 
+void get_line(std::string const &name, std::string &result);
+std::string hide_password(std::string const &password);
+
 enum
 {
-    max_length = 1024
+    max_length = 16 * 1024
 };
 
 int main(int argc, char *argv[])
 {
+    std::string username = "";
+    std::string password = "";
+    std::string str_request = "";
+
     try
     {
         if (argc != 3)
@@ -59,6 +66,17 @@ int main(int argc, char *argv[])
             std::cerr << "Usage: sbc-client <host> <port>\n";
             return 1;
         }
+
+        std::cout << "Enter your username and password" << std::endl;
+
+        get_line("username: ", username);
+        get_line("password: ", password);
+
+        std::cout << std::endl << "username: [" << username << "] password: [" << hide_password(password) << "]" << std::endl << std::endl;
+
+        get_line("request: ", str_request);
+
+        std::cout << std::endl << "request: [" << str_request << "]" << std::endl << std::endl;
 
         boost::asio::io_service io_service;
 
@@ -69,9 +87,17 @@ int main(int argc, char *argv[])
         tcp::socket s(io_service);
         boost::asio::connect(s, iterator);
 
-        std::cout << "Enter message: ";
+        // std::cout << "Enter message: ";
+        // char request[max_length];
+        // std::cin.getline(request, max_length);
+        // size_t request_length = std::strlen(request);
+        // boost::asio::write(s, boost::asio::buffer(request, request_length));
+
         char request[max_length];
-        std::cin.getline(request, max_length);
+        for (int i = 0; i < str_request.size && i < max_length; i++)
+        {
+            request[i] = str_request[i];
+        }
         size_t request_length = std::strlen(request);
         boost::asio::write(s, boost::asio::buffer(request, request_length));
 
@@ -87,4 +113,28 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+/* Получаем строку от пользователя */
+void get_line(std::string const &name, std::string &result)
+{
+    for (std::string line; std::cout << name && std::getline(std::cin, line);)
+    {
+        if (!line.empty())
+        {
+            result = line;
+            break;
+        }
+    }
+}
+
+/* Скрываем пароль при выводе */
+std::string hide_password(std::string const &password)
+{
+    std::string result = "";
+    for (int i = 0; i < password.size(); i++)
+    {
+        result += "*";
+    }
+    return result;
 }
