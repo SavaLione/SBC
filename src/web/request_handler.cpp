f7d4225a0086f1dab33eae5615ba3679faccf387
SavaLione <monologuesplus@gmail.com>
2021-01-15 15:50:11 +0500

    Баг с главной страницей

--- src/web/request_handler.cpp
+++ src/web/request_handler.cpp
@@ -180,8 +180,8 @@ void request_handler::_show_page_dashboard()
 
 void request_handler::_show_page_index()
 {
-    index i = index(_user);
-    _show_page(i);
+    // index i = index(_user);
+    // _show_page(i);
 }
 
 void request_handler::_recognize_user()
